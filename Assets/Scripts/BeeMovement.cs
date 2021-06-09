515bf60f3b017915668ac637729990e865654545
TheDerz12 <axerickson12@yahoo.com>
2021-06-09 10:57:47 +0200

    Attempted world border

--- Assets/Scripts/BeeMovement.cs
+++ Assets/Scripts/BeeMovement.cs
@@ -4,29 +4,22 @@ using UnityEngine;
 
 public class BeeMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
+
     public float speed;
-    public float accelerationTime = 2f;
-    public float maxSpeed = 5f;
-    private Vector2 movement;
-    private float timeLeft;
+    public Rigidbody2D rb;
+
+   
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
-    void Update()
-    {
-        timeLeft -= Time.deltaTime;
-        if (timeLeft <= 0)
-        {
-            movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-            timeLeft += accelerationTime;
-        }
+
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        rb.AddForce(movement * maxSpeed);
+
+        rb.velocity = new Vector2(transform.position.x * speed, transform.position.y * speed);
+
     }
 }
